cf938f74a8d19a80d2d1e07d94c9c097c8674d64
Ruqsar Firdous <ruqsarfirdous110@gmail.com>
2025-11-26 14:15:29 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,16 +5,45 @@ import pandas as pd
 import numpy as np
 import dill
 
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+
 from src.exception import CustomException
 
-def save_object(file_path,obj):
+
+def save_object(file_path, obj):
     try:
-        dir_path=os.path.dirname(file_path)
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
 
-        os.makedirs(dir_path,exist_ok=True)
-
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+
+
+
+def evaluate_models(X_train, X_test, y_train, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)
+            
+            # Predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
